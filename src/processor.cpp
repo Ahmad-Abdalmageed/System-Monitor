0af26fe556e103e4f2ccb0c4f541aed6e906f68a
Ahmad <ahmedmageed19@gmail.com>
2022-07-25 16:05:16 +0200

    update(Processor): Updated the PRocessor Clas Implementaiton

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,15 @@
 #include "processor.h"
-
+#include "linux_parser.h"
 // TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+float Processor::Utilization()
+{
+    // Calculate Deltas of Active and Idle Times
+    float activeJiffiesDelta = LinuxParser::ActiveJiffies() - prevReadActiveJiffies_;
+    float idleJiffiesDelta = LinuxParser::IdleJiffies() - prevReadIdleJiffies_;
+
+    // Update the Previous Readings
+    prevReadActiveJiffies_ = LinuxParser::ActiveJiffies();
+    prevReadIdleJiffies_ = LinuxParser::IdleJiffies();
+
+    return activeJiffiesDelta - idleJiffiesDelta / activeJiffiesDelta;
+}
