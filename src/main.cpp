4f706c8e23fef06aafd3693b9a93cba137242506
Ahmad <ahmedmageed19@gmail.com>
2022-08-06 22:23:46 +0200

    refactor(main): Removed Testing Functions --- not needed

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,11 @@
 #include "ncurses_display.h"
 #include "system.h"
 
-// Testing .. TODO: REMOVE After Finish
-#include "linux_parser.h"
-#include <iostream>
-#include <iomanip>
-
 void test();
 
 int main()
 {
   // Main System Program
-  // System system;
-  // NCursesDisplay::Display(system);
-
-  // Testing Functionality
-  test();
-}
-
-void test()
-{
-  std::cout << LinuxParser::IdleJiffies() << "\n";
-  std::cout << LinuxParser::TotalProcesses() << "\n";
+  System system;
+  NCursesDisplay::Display(system);
 }
